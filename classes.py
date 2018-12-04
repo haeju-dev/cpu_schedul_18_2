c4ee9b97fa551182715962ced21c8d4c0aa3e288
hwanghaeju <haeju.dev@gmail.com>
2018-12-05 04:01:57 +0900

    FIN

--- classes.py
+++ classes.py
@@ -1,85 +1,97 @@
-class process:
+class process:  # 프로세스
     def __init__(self, pid, priority, arrive_time, burst_time):
-        self.is_ended = False
-        self.end_time = -1
-        self.burst_time = burst_time
-        self.rest = burst_time
         self.pid = pid
         self.priority = priority
-        self.arrive_time = arrive_time
+        self.arrive_time = arrive_time  # 해당 프로세스가 큐에 등록될 clock
+        self.start_time = -1  # (Debug) 해당 프로세스의 수행이 실질적으로 시작된 clock
+        self.burst_time = burst_time
+        self.remaining = burst_time  # 해당 프로세스의 남아있는 시간 (Remaining), burst로 초기화
+        self.is_ended = False  # 해당 프로세스의 작업 종료 여부
+        self.end_time = -1  # 해당 프로세스가 끝날 경우 끝난 clock + 1이 세팅될 예정
+
+    @property  # 속성값, 해당 인스턴스 호출시 자동으로 이 method의 반환값을 제공한다
+    def waiting_time(self):  # 해당 프로세스의 waiting time
+        return self.end_time - self.arrive_time - self.burst_time
+
+    def do(self, clock):  # 해당 프로세스를 한 클럭씩 수행하는 시뮬레이션 역할을 담당하는 method
+        if self.start_time == -1:  # (Debug) 실질적인 실행이 처음 시작된 경우
+            self.start_time = clock  # (Debug) 현재 clock을 저장
+        self.remaining -= 1  # 프로세스의 remaining time을 감소시킨다. (1 clock 실행한다)
+        if not self.remaining:  # 프로세스의 remaining time이 없을 경우 == 프로세스의 실행이 모두 끝날 경우
+            self.finish(clock)  # 프로세스 종료 method를 실행한다
+
+    def finish(self, clock):  # 프로세스 종료 method
+        self.end_time = clock + 1  # 프로세스가 종료된 다음 clock을 end time에 저장
+        self.is_ended = True  # 종료 여부 체크
 
     @property
-    def wating_time(self):
-        return self.end_time - self.arrive_time + 1
+    def output(self):  # 해당 프로세스의 시뮬레이션 결과 출력용 instance
+        return f"{self.pid} {self.waiting_time}"  # process number와 waiting time을 문자열 포맷으로 반환한다
 
-    def do(self, clock):
-        self.rest -= 1
-        if not self.rest:
-            self.finish(clock)
-            return self
-        return self
-
-    def finish(self, clock):
-        self.end_time = clock
-        self.is_ended = True
-
-    def __str__(self):
-        return f"pid : {self.pid} is_ended : {self.is_ended} end_time : {self.end_time}" + f"\n{self.rest}"
+    def __str__(self):  # (Debug) 디버그 화면에서 클래스 정보 전달을 용이하게 하기 위함
+        return f"pid:{self.pid} start:{self.start_time} end:{self.end_time} arrival:{self.arrive_time} " + \
+               f"burst:{self.burst_time} waiting:{self.waiting_time}"
 
 
-class rq:
+class rq:  # Ready Queue의 공통된 부분을 정의해둔 부모 클래스
     name = 'Ready Queue'
-    process = list()
-    ended = list()
-    current = None
 
-    def status(self):
+    def __init__(self):  # 생성자 (Constructor)
+        self.process = list()  # 해당 큐의 남아있는 프로세스의 리스트
+        self.current = None  # 해당 큐에서 현재 실행중인 프로세스
+        self.ended = list()  # 해당 큐에서 완료된 프로세스의 리스트
+
+    @property
+    def avg_waiting_inqueue(self):  # (Debug) 해당 큐에서 완료된 프로세스의 평균 waiting time
+        n = 0
+        for i in self.ended:
+            n += i.waiting_time
+        if len(self.ended):  # (Debug) zero-division Error 방지
+            return n / len(self.ended)
+        return n
+
+    def status(self):  # 해당 큐에서 실행중이거나 실행 가능한 프로세스의 존재 여부를 반환하는 method
         if len(self.process) or not (self.current is None):
             return True
-        return False
+        return False  # 해당 큐가 비어있음을 의미
 
-    def do(self, clock):
-        if self.current is None:
-            current = self.process.pop(0)
+    def do(self, clock):  # 해당 큐의 프로세스를 실행하는 method
+        if self.current is None:  # 현재 실행중인 프로세스가 없다면
+            current = self.process.pop(0)  # 실행해야할 프로세스의 가장 첫번째를 가져온다
         else:
-            current = self.current
-        current.do(clock)
-        if current.is_ended:
-            self.ended.append(current)
-            self.current = None
+            current = self.current  # 현재 실행중인 프로세스가 있다면 해당 프로세스를 가져온다
+        current.do(clock)  # 프로세스를 한 clock 실행한다 -> 프로세스의 remaining time을 줄이고, 만약 0이 된다면 종료 method를 실행한다
+        if current.is_ended:  # 프로세스가 끝났을 경우
+            self.ended.append(current)  # 현재 큐의 완료된 프로세스 리스트에 해당 프로세스를 추가한다
+            self.current = None  # 현재 실행중인 프로세스를 담는 공간을 비운다
         else:
-            self.current = current
+            self.current = current  # 프로세스가 끝나지 않았을 경우 현재 실행중인 프로세스로 저장하고 다음 순서를 기다린다
 
-    def sort(self):
+    def sort(self):  # 실행할 프로세스 리스트를 정렬하는 method, 각 큐에서 override해 사용할 예정
         pass
 
 
 class fcfs(rq):
-    name = 'FCFS'
+    name = 'FCFS'  # 먼저 들어온 프로세스를 먼저 처리하는 큐
 
-    def sort(self):
+    def sort(self):  # arrive_time 기준으로 정렬하려고 했으나 input 받아올때 하므로 의미없음
         # self.process = sorted(self.process, key=lambda pc: pc.arrive_time)
         pass
 
 
 class sjf(rq):
-    name = 'SJF'
+    name = 'SJF'  # (non-preemptive) Shotest Job First, burst_time이 가장 낮은 프로세스부터 처리한다
 
-    def sort(self):
-        self.process = sorted(self.process, key=lambda pc: pc.rest)
+    def sort(self):  # burst_time 기준으로 실행해야할 프로세스를 정렬한다 (현재 실행중인 프로세스는 따로 관리되므로 비선점형)
+        self.process = sorted(self.process, key=lambda pc: pc.burst_time)
         pass
 
 
 class rr(rq):
-    name = 'RR'
-    isinquantum = False
+    name = 'RR'  # Round Robin
+    isinquantum = False  # 실행중인 블록의 중간에 위치해 있는지를 체크하는 instance, quantum size가 2이므로 boolean형
 
-    def status(self):
-        if len(self.process) or not (self.current is None) or not self.isinquantum:
-            return True
-        return False
-
-    def do(self, clock):
+    def do(self, clock):  # do method를 override
         if self.current is None:
             current = self.process.pop(0)
         else:
@@ -87,39 +99,44 @@ class rr(rq):
 
         current.do(clock)
 
-        if current.is_ended:
+        if current.is_ended: # 만약 프로세스가 완료될경우
             self.ended.append(current)
             self.current = None
-            self.isinquantum = False
+            self.isinquantum = False # 실행중인 블록을 초기화
         else:
-            if self.isinquantum:
-                self.process.append(current)
+            if self.isinquantum: # 만약 현재 프로세스의 실행 count가 quantum size에 도달했다면
+                self.process.append(current) # 실행할 프로세스 리스트의 맨 뒤로 보낸다
                 self.current = None
-                self.isinquantum = False
-            elif not self.isinquantum:
-                self.isinquantum = True
+                self.isinquantum = False # 실행중인 블록 정보를 초기화
+            elif not self.isinquantum: # 만약 현재 프로세스의 실행 count가 quantum size에 도달하지 않았다면
+                self.isinquantum = True # 현재 프로세스의 실행 count를 증가시킨다
                 self.current = current
 
-    def _2qdo(self, clock):
-        if self.isinquantum:
-            if self.current is None:
-                self.isinquantum = not self.isinquantum
-                return None
-            else:
-                current = self.current
-        else:
-            if self.current is None:
-                current = self.process.pop(0)
-            else:
-                current = self.current
+    # def status(self):
+    #     if len(self.process) or not (self.current is None) or self.isinquantum:
+    #         return True
+    #     return False
 
-        current.do(clock)
-
-        if current.is_ended:
-            self.ended.append(current)
-            self.current = None
-            self.isinquantum = not self.isinquantum
-        else:
-            self.current = current
-            self.isinquantum = not self.isinquantum
-        return None
+    # def _2qdo(self, clock):
+    #     if self.isinquantum:
+    #         if self.current is None:
+    #             self.isinquantum = not self.isinquantum
+    #             return None
+    #         else:
+    #             current = self.current
+    #     else:
+    #         if self.current is None:
+    #             current = self.process.pop(0)
+    #         else:
+    #             current = self.current
+    #
+    #     current.do(clock)
+    #
+    #     if current.is_ended:
+    #         self.ended.append(current)
+    #         self.current = None
+    #         self.isinquantum = not self.isinquantum
+    #     else:
+    #         self.current = current
+    #         self.isinquantum = not self.isinquantum
+    #     return None
--- input.py
+++ input.py
@@ -1,6 +1,6 @@
 from classes import process
 
-
+# 필터
 def filter_word(character):
     NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ']
     # 입력받은 문자가 위 리스트에 있으면 True
@@ -9,47 +9,42 @@ def filter_word(character):
     else:
         return False
 
-
+# Raw Data를 파일에서 받아와 리스트로 변환하는 함수
 def getrawinput(dir):
     f = open(dir, 'r')
     data = list()
     while True:
         tmp = f.readline()
-        if tmp == '':
+        if tmp == '': # EOF시 종료
             break
-        tmp = tmp.strip()
+        tmp = tmp.strip() # 양옆의 공백 제거
 
         newstr = ''
-        for i in filter(filter_word, tmp):
+        for i in filter(filter_word, tmp): # 입력값에서 필터함수를 적용해 숫자와 공백만 추출
             newstr += i
 
-        while '  ' in newstr:
+        while '  ' in newstr: # 공백이 여러개 붙어있을 경우 공백이 하나가 될때까지 병합
             newstr = newstr.replace('  ', ' ')
         data.append(newstr)
     f.close()
     return data
 
-
-def makedataset(raw):
-    # return {raw[0]: makeprocess(raw)}
-    return makeprocess(raw)
-
-
+# Dataset으로부터 Process 클래스를 생성하는 함수
 def makeprocess(dataset):
     return process(pid=int(dataset[0]), priority=int(dataset[1]),
                    arrive_time=int(dataset[2]), burst_time=int(dataset[3]))
 
-
-def getinput(dir, spawner=makedataset):
-    raw = getrawinput(dir)
+# 실질적인 Input을 종합 처리하는 함수, spawner parameter로 입력된 함수를 통해 데이터셋을 리스트로 반환한다.
+def getinput(dir, spawner=makeprocess):
+    raw = getrawinput(dir) # Raw Data
     data = list()
-    for i in raw:
-        splitted = i.split(' ')
-        if not len(splitted) == 4:
+    for i in raw: # Raw Data list 각각에서 하나씩
+        splitted = i.split(' ') # 공백으로 구분하여
+        if not len(splitted) == 4: # 구분된 토큰 갯수가 4개가 아니면 에러
             raise Exception('Input data is invalid')
-        data.append(spawner(splitted))
-    return sorted(data, key=lambda pc: pc.arrive_time)
+        data.append(spawner(splitted)) # 에러가 안날경우 data 리스트에 spawner에서 반환된 클래스를 추가
+    return sorted(data, key=lambda pc: pc.arrive_time) # arrive_time으로 정렬해 반환
 
-if __name__=="__main__":
-    data = getinput('process.in', spawner=makedataset)
+if __name__=="__main__": # (Debug) 실행시간에는 관계없음
+    data = getinput('process.in', spawner=makeprocess)
     print(data)
